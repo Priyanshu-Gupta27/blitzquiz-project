034acae2b2a511e3a987f128314f53d1d6ea03f7
Priyanshu-Gupta27 <priyan969666@gmail.com>
2026-05-30 13:38:53 +0530

    Test homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import Navbar from '@/components/layout/Navbar';
-import Hero from '@/components/sections/Hero';
-import Stats from '@/components/sections/Stats';
-import ActiveContests from '@/components/sections/ActiveContests';
-import Features from '@/components/sections/Features';
-import Footer from '@/components/layout/Footer';
-
 export default function Home() {
   return (
     <main>
-      <Navbar />
-      <Hero />
-      <Stats />
-      <ActiveContests />
-      <Features />
-      <Footer />
+      <h1>Hello BlitzQuiz</h1>
     </main>
   );
 }
